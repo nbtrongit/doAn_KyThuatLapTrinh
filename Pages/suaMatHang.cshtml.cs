7403b4b1d03337d91611105fac57cb2e0c63c2b1
Trong <Trong@>
2022-05-03 23:50:31 +0700

    sửa 3

---
+++ Pages/suaMatHang.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using doAn_KTLT.Entities;
+using doAn_KTLT.Services;
+
+namespace doAn_KTLT.Pages
+{
+    public class suaMatHangModel : PageModel
+    {
+        public MatHang matHang;
+        public List<string> dsLoaiHang;
+        public string ketQua;
+        public bool kiemTra;
+        [BindProperty(SupportsGet = true)]
+        public string id { get; set; }
+        [BindProperty]
+        public string maHang { get; set; }
+        [BindProperty]
+        public string tenMH { get; set; }
+        [BindProperty]
+        public string hanDung { get; set; }
+        [BindProperty]
+        public string congtySX { get; set; }
+        [BindProperty]
+        public string ngaySX { get; set; }
+        [BindProperty]
+        public int donGia { get; set; }
+        [BindProperty]
+        public string loaiHang { get; set; }
+        public void OnGet()
+        {
+            dsLoaiHang = XuLyLoaiHang.timKiem(string.Empty);
+            MatHang? mh = XuLyMatHang.timMatHang(id);
+            if (mh != null)
+            {
+                matHang = mh.Value;
+            }
+            else
+            {
+                ketQua = "Không tìm thấy mặt hàng";
+            }
+            kiemTra = (mh != null);
+        }
+        public void OnPost()
+        {
+            dsLoaiHang = XuLyLoaiHang.timKiem(string.Empty);
+            matHang.maHang = maHang;
+            matHang.tenHang = tenMH;
+            matHang.hanDung = XuLyDate.phanGiai(hanDung);
+            matHang.congTySanXuat = congtySX;
+            matHang.ngaySanXuat = XuLyDate.phanGiai(ngaySX);
+            matHang.donGia = donGia;
+            matHang.loaiHang = loaiHang;
+            bool kq = XuLyMatHang.Sua(id, matHang);
+            if (kq == true)
+            {
+                Response.Redirect("/MatHang");
+            }
+            else
+            {
+                ketQua = "Xóa: False";
+            }
+        }
+    }
+}
--- Services/XuLyMatHang.cs
+++ Services/XuLyMatHang.cs
@@ -87,5 +87,107 @@ namespace doAn_KTLT.Services
             LuuTruMatHang.Luu(dsMatHang);
             return true;
         }
+        public static bool Sua (string id, MatHang matHangMoi)
+        {
+            List<HoaDonBan> dsHoaDonBan = LuuTruHoaDonBan.Doc();
+            List<HoaDonNhap> dsHoaDonNhap = LuuTruHoaDonNhap.Doc();
+            List<MatHang> dsMatHang = LuuTruMatHang.Doc();
+            List<string> dsLoaiHang = LuuTruLoaiHang.Doc();
+            MatHang matHang = dsMatHang[0];
+            foreach(MatHang mh in dsMatHang)
+            {
+                if(mh.maHang == id)
+                {
+                    matHang = mh;
+                    break;
+                }
+            }
+            for(int i = 0; i < dsHoaDonNhap.Count; i++)
+            {
+                if(dsHoaDonNhap[i].maHang != id && matHangMoi.maHang == dsHoaDonNhap[i].maHang)
+                {
+                    return false;
+                }
+                else if(dsHoaDonNhap[i].maHang == id)
+                {
+                    if (XuLyDate.chuyenDoi(matHangMoi.hanDung) <= XuLyDate.chuyenDoi(dsHoaDonNhap[i].ngayHoaDon) || XuLyDate.chuyenDoi(matHangMoi.ngaySanXuat) >= XuLyDate.chuyenDoi(dsHoaDonNhap[i].ngayHoaDon))
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (XuLyDate.chuyenDoi(matHangMoi.hanDung) <= XuLyDate.chuyenDoi(matHangMoi.ngaySanXuat))
+            {
+                return false;
+            }
+            for (int i = 0; i < dsHoaDonBan.Count; i++)
+            {
+                if(dsHoaDonBan[i].maHang == id && XuLyDate.chuyenDoi(matHangMoi.hanDung) <= XuLyDate.chuyenDoi(dsHoaDonBan[i].ngayHoaDon))
+                {
+                    return false;
+                }
+                else if (dsHoaDonBan[i].maHang == id && matHangMoi.donGia >= dsHoaDonBan[i].donGia)
+                {
+                    return false;
+                }
+            } 
+            if(matHangMoi.donGia == 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < dsLoaiHang.Count; i++)
+            {
+                if(dsLoaiHang[i] != id && dsLoaiHang[i] == matHangMoi.loaiHang)
+                {
+                    return false;
+                }
+            }
+            for (int i = 0; i < dsMatHang.Count; i++)
+            {
+                if(dsMatHang[i].maHang == id)
+                {
+                    dsMatHang[i] = matHangMoi;
+                }
+                if(dsMatHang[i].loaiHang == matHang.loaiHang)
+                {
+                    MatHang mh = dsMatHang[i];
+                    mh.loaiHang = matHangMoi.loaiHang;
+                    dsMatHang[i] = mh;
+                }
+            }
+            LuuTruMatHang.Luu(dsMatHang);
+            for (int i = 0; i < dsLoaiHang.Count; i++)
+            {
+                if (dsLoaiHang[i] == matHang.loaiHang)
+                {
+                    dsLoaiHang[i] = matHangMoi.loaiHang;
+                    break;
+                }
+            }
+            LuuTruLoaiHang.Luu(dsLoaiHang);
+            for (int i = 0; i < dsHoaDonNhap.Count; i++)
+            {
+                if(dsHoaDonNhap[i].maHang == id)
+                {
+                    HoaDonNhap hdn = dsHoaDonNhap[i];
+                    hdn.maHang = matHangMoi.maHang;
+                    hdn.donGia = matHangMoi.donGia;
+                    dsHoaDonNhap[i] = hdn;
+                    break;
+                }
+            }
+            LuuTruHoaDonNhap.Luu(dsHoaDonNhap);
+            for (int i = 0; i < dsHoaDonBan.Count; i++)
+            {
+                if(dsHoaDonBan[i].maHang == id)
+                {
+                    HoaDonBan hdb = dsHoaDonBan[i];
+                    hdb.maHang = matHangMoi.maHang;
+                    dsHoaDonBan[i] = hdb;
+                }
+            }
+            LuuTruHoaDonBan.Luu(dsHoaDonBan);
+            return true;
+        }
     }
 }
